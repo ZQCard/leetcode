dc1b9c3fc4b162cf325ce2dc6247a6f0d655bacc
zhouqi445864742 <445864742@qq.com>
2018-11-21 20:05:40 +0800

    'push'

--- problem/low/088/merge_sorted_array.go
+++ problem/low/088/merge_sorted_array.go
@@ -16,3 +16,25 @@ func merge(nums1 []int, m int, nums2 []int, n int)  {
 		}
 	}
 }
+
+func TheBest(nums1 []int, m int, nums2 []int, n int)  {
+	cur := m + n - 1
+	p1, p2 := m - 1, n - 1
+	for p1 >= 0 && p2 >= 0 {
+		if nums1[p1] > nums2[p2] {
+			nums1[cur] = nums1[p1]
+			p1--
+		} else {
+			nums1[cur] = nums2[p2]
+			p2--
+		}
+
+		cur--
+	}
+
+	for p2 >= 0 {
+		nums1[cur] = nums2[p2]
+		p2--
+		cur--
+	}
+}
--- problem/low/088/merge_sorted_array_test.go
+++ problem/low/088/merge_sorted_array_test.go
@@ -3,7 +3,7 @@ package merge_sorted_array
 import "testing"
 
 func TestMergeSortArray(t *testing.T)  {
-	num1 := []int{1,2,3,0,0,0}
+	num1 := []int{4,5,6,0,0,0}
 	num2 := []int{2,5,6}
-	merge(num1, 3, num2, 3)
+	TheBest(num1, 3, num2, 3)
 }
